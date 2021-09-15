6f04320c28e16c3c56a02a2c64c77ecbd87fe854
berrygxxd <88806842+berrygxxd@users.noreply.github.com>
2021-09-15 19:29:14 +0900

    JS : Display items

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+'use strict';
 // Fetch the items from the JSON file
 function loadItems() {
     return fetch('data/data.json')
@@ -6,11 +6,27 @@ function loadItems() {
     .then(json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 //main
 loadItems()
 .then(items => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items)
 })
 .catch(console.log);
